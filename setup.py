d52a2af36bc94c7323f55d7078c3d9215bfedbd1
dceoy <dnarsil+github@gmail.com>
2021-01-01 00:26:21 +0900

    Publish the package to PyPI using an API token

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     ),
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='git@github.com:dceoy/ftarc.git',
+    url='https://github.com/dceoy/ftarc',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
